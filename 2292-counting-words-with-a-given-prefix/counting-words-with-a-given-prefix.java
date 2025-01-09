0cf647bf3f48160ae16c94ad957b842023d1a5fb
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-01-09 17:25:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (69.98%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -1,24 +1,38 @@
+// class Solution {
+//     public int prefixCount(String[] words, String pref) {
+//        int n = words.length;
+//        int m = pref.length();
+//        int count = 0;
+//        for(int i=0; i<n; i++)
+//        {
+//         count += counterFun(words[i], pref, m);
+//        }
+//        return count;
+//     }
+
+//     public int counterFun(String word, String pref, int m){
+//         if (word.length() < m) {
+//             return 0;
+//         }
+//        for (int i = 0; i < m; i++) {
+//             if (word.charAt(i) != pref.charAt(i)) {
+//                 return 0;
+//             }
+//         }
+//         return 1;
+//     }
+// }
+
 class Solution {
     public int prefixCount(String[] words, String pref) {
-       int n = words.length;
-       int m = pref.length();
-       int count = 0;
-       for(int i=0; i<n; i++)
-       {
-        count += counterFun(words[i], pref, m);
-       }
-       return count;
-    }
-
-    public int counterFun(String word, String pref, int m){
-        if (word.length() < m) {
-            return 0;
-        }
-       for (int i = 0; i < m; i++) {
-            if (word.charAt(i) != pref.charAt(i)) {
-                return 0; // Mismatch found, not a prefix
+        int c=0;
+        for(int i=0;i<words.length;i++)
+        {
+            if(words[i].startsWith(pref))
+            {
+                c++;
             }
         }
-        return 1;
+        return c;
     }
 }
