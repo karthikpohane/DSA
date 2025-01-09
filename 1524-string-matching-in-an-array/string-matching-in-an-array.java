48118a12a65bfd6aeac85ded2bf04185ee8b3e7a
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-01-09 16:50:56 +0530

    Time: 21 ms (6.03%) | Memory: 45.5 MB (5.60%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,15 +1,15 @@
+import java.util.Vector;
+
+// KMP Algorithm
 class Solution {
     public List<String> stringMatching(String[] words) {
         int n = words.length;
         List<String> result = new ArrayList<>();
 
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if(i==j) continue;
-                if(words[j].contains(words[i]))
-                {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                if (searchKMP(words[i], words[j])) {
                     result.add(words[i]);
                     break;
                 }
@@ -17,4 +17,84 @@ class Solution {
         }
         return result;
     }
+
+    // KMP search function
+    public Boolean searchKMP(String pat, String txt) {
+        int N = txt.length();
+        int M = pat.length();
+
+        // Find the LPS array
+        Vector<Integer> LPS = new Vector<>(M);
+        for (int i = 0; i < M; i++) LPS.add(0); // Initialize with zeros
+        computeLPS(pat, LPS, M);
+
+        // Apply KMP algorithm
+        int i = 0, j = 0;
+        while (i < N) {
+            if (txt.charAt(i) == pat.charAt(j)) {
+                i++;
+                j++;
+            }
+            if (j == M) {
+                return true; // Pattern found
+            } else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+                if (j != 0) {
+                    j = LPS.get(j - 1);
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false; // Pattern not found
+    }
+
+    // Compute LPS array
+    public void computeLPS(String pat, Vector<Integer> LPS, int M) {
+        int len = 0;
+        LPS.set(0, 0); // LPS[0] is always 0
+        int i = 1;
+
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                LPS.set(i, len);
+                i++;
+            } else {
+                if (len != 0) {
+                    len = LPS.get(len - 1);
+                } else {
+                    LPS.set(i, 0);
+                    i++;
+                }
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+//Brutforce Method
+// class Solution {
+//     public List<String> stringMatching(String[] words) {
+//         int n = words.length;
+//         List<String> result = new ArrayList<>();
+
+//         for(int i=0; i<n; i++)
+//         {
+//             for(int j=0; j<n; j++)
+//             {
+//                 if(i==j) continue;
+//                 if(words[j].contains(words[i]))
+//                 {
+//                     result.add(words[i]);
+//                     break;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// }
