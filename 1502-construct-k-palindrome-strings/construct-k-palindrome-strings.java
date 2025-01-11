bc30f64adfaf36b36137e9cfac83fc20a46c9732
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-01-11 10:28:53 +0530

    Time: 4 ms (94.60%) | Memory: 45.4 MB (45.32%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean canConstruct(String s, int k) {
         int n = s.length();
-        if(n==k)
-            return true;
-        if(n<k)
-            return false;
+        
+        if(n==k) return true;
+        if(n<k)  return false;
+
         //If k is less than odd frequence charecters then return false.
         //i.e is if(odd freq chars > k) return false
         //if(odd freq chars <= k) return true.
+        
         int [] freq = new int[26];
         for(char ch: s.toCharArray())
         {
@@ -20,8 +21,9 @@ class Solution {
             if(freq[i]%2 != 0)
                 oddFreqChars++;
         }
-        if(oddFreqChars>k)
-            return false;
-        else return true;
+        // if(oddFreqChars>k)
+        //     return false;
+        // else return true;
+        return oddFreqChars<=k;
     }
 }
