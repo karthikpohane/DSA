8be2163f20464d9d6c8b5c16f25106bbf6d9d3e8
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-01-16 15:11:38 +0530

    Time: 1 ms (100.00%) | Memory: 61.5 MB (47.41%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.java
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.java
@@ -2,22 +2,21 @@ class Solution {
     public int xorAllNums(int[] nums1, int[] nums2) {
        int n = nums1.length;
        int m = nums2.length; 
-
-       Map<Integer, Integer> mp = new HashMap<>();
-       for(int num: nums1)
-       {
-        mp.put(num, mp.getOrDefault(num, 0)+m);
-       }
-       for(int num: nums2)
-       {
-        mp.put(num, mp.getOrDefault(num, 0)+n);
-       }
-       
        int result = 0;
-       for(int num: mp.keySet())
+
+       if(n%2!=0)
        {
-        if(mp.get(num) % 2 != 0)
-            result^=num;
+        for(int num: nums2)
+        {
+            result ^= num;
+        }
+       }
+       if(m%2!=0)
+       {
+        for(int num: nums1)
+        {
+            result ^= num;
+        }
        }
        return result;
     }
