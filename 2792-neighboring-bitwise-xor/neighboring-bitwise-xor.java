0f2a882a83cd43a98dbb429c1837591bc7ceb0eb
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-01-17 12:17:35 +0530

    Time: 4 ms (36.47%) | Memory: 60.4 MB (81.18%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
         int n = derived.length;
+        int[] org = new int[n];
 
         if (n == 1) {
             return derived[0] == 0;
         }
-
-        for (int j = 0; j < 2; j++) {
-            int[] org = new int[n];
-            org[0] = j; 
+        org[0] = 0; 
            
             for (int i = 1; i < n; i++) {
                 org[i] = org[i - 1] ^ derived[i - 1];
@@ -18,7 +16,17 @@ class Solution {
             if ((org[n - 1] ^ org[0]) == derived[n - 1]) {
                 return true;
             }
-        }
+        
+        org[0] = 1; 
+           
+            for (int i = 1; i < n; i++) {
+                org[i] = org[i - 1] ^ derived[i - 1];
+            }
+
+            
+            if ((org[n - 1] ^ org[0]) == derived[n - 1]) {
+                return true;
+            }
 
         return false; 
     }
