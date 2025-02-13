12fa1ea0b0830651682dafdb5174090b4244ae4f
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-02-13 15:26:06 +0530

    Time: 2 ms (62.80%) | Memory: 42.4 MB (90.19%) - LeetSync

--- 3447-clear-digits/clear-digits.java
+++ 3447-clear-digits/clear-digits.java
@@ -1,19 +1,14 @@
 class Solution {
     public String clearDigits(String s) {
-        Stack<Character> st = new Stack<Character>();
         StringBuffer temp = new StringBuffer();
         for(int i=0; i<s.length(); i++){
             if(!Character.isDigit(s.charAt(i))){
-                st.push(s.charAt(i));
-            }else{
-                if(!st.isEmpty())
-                st.pop();
+                temp.append(s.charAt(i));
+            }else if(temp.length()>0){
+                temp.deleteCharAt(temp.length()-1);
             }
         }
-        while(!st.isEmpty()) {
-            temp.append(st.pop());
-        }
-        return temp.reverse().toString();
+        return temp.toString();
 
     }
 }
