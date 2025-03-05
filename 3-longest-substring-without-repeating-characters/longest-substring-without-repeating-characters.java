8cb64be17c92267c0d994b4c1d6390f0aa92cdc3
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-03-05 19:29:21 +0530

    Time: 2 ms (98.59%) | Memory: 43.7 MB (87.82%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length()==1) return 1;
+        int[] arry = new int[255];
+        for(int i=0; i<arry.length; i++){
+            arry[i] = -1;
+        }
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+        while(right<s.length()){
+            if(arry[s.charAt(right)] != -1){
+                if(arry[s.charAt(right)]>=left){
+                    left = arry[s.charAt(right)] + 1;
+                }
+            }
+            int len = right-left+1;
+            maxLen = Math.max(len, maxLen);
+            arry[s.charAt(right)] = right;
+            right++;
+        }
+        return maxLen;
+    }
+}
