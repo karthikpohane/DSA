48add75b55ba0749d28c38314117f84894cb8e37
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-03-11 19:48:39 +0530

    Time: 1 ms (99.49%) | Memory: 56.9 MB (80.91%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,12 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum = 0;
-        int maxSum = nums[0];
-        for(int i=0;i<nums.length; i++){
+        //Use of Kaden's Algo - Where you just keep on adding in sum and,
+        //if the sum becomes less then zero then make the sum as Zero.
+
+        int sum = nums[0];
+        int maxLen = nums[0];
+        for(int i=1; i<nums.length; i++){
             if(sum<0) sum = 0;
             sum += nums[i];
-            maxSum = Math.max(sum, maxSum);
+            maxLen = Math.max(sum, maxLen);
         }
-        return maxSum;
+        return maxLen;
     }
 }
