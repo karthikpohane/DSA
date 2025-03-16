8dac42882a3dd73237213e46838aa166fcf2dd41
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-03-16 12:45:10 +0530

    Time: 2 ms (28.74%) | Memory: 49.3 MB (8.82%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -9,71 +9,11 @@
  *     }
  * }
  */
-
-//Approach One
-// public class Solution {
-//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-//         HashMap<ListNode, Integer> map = new HashMap<>();
-//         ListNode temp = headA;
-//         while(temp!=null){
-//             map.put(temp, 1);
-//             temp = temp.next;
-//         }
-//         temp = headB;
-//         while(temp!=null){
-//             if(map.containsKey(temp))
-//                 return temp;
-//             temp = temp.next;
-//         }
-//         return null;
-//     }
-// }
-
-//Approach Two
-// public class Solution {
-//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-//         ListNode temp1 = headA;
-//         ListNode temp2 = headB;
-//         int len1 = 0;
-//         int len2 = 0;
-//         while(temp1!=null){
-//             len1++;
-//             temp1 = temp1.next;
-//         }
-//         while(temp2!=null){
-//             len2++;
-//             temp2 = temp2.next;
-//         }
-//         if(len1<len2){
-//             int diff = len2-len1;
-//             return spotLink(headA, headB, diff);
-//         } else{
-//             int diff = len1-len2;
-//             return spotLink(headB, headA, diff);
-//         } 
-//     }
-    
-//     public ListNode spotLink(ListNode temp1, ListNode temp2, int diff){
-//         while(diff!=0){
-//             diff--;
-//             temp2 = temp2.next;
-//         }
-
-//         while(temp1 != temp2){
-//             temp1 = temp1.next;
-//             temp2 = temp2.next;
-//         }
-//         return temp2;
-//     } 
-// }
-
-//Optimal Approach
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode temp1 = headA;
         ListNode temp2 = headB;
-
-        while(temp1 != temp2){
+        while(temp1!=temp2){
             if(temp1 == null) temp1 = headB;
             if(temp2 == null) temp2 = headA;
             if(temp1 == temp2) return temp2;
