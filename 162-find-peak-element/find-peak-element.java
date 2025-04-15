2addfb685c228a648aa85cd5cdc8e944e57bce9a
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-04-15 19:25:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (56.49%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,25 +1,25 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int peak = -1;
-        if(nums.length==1) return 0;
-        if(nums.length <=2) {
-            if(nums[0]<nums[1]) return 1;
-            else return 0;
+        //Using Binary search
+        //Step 1 - finding the mid and checking if that is the peak ele.
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        if(n==1) return 0;
+        else if(nums[low]>nums[low+1]) return low;
+        else if(nums[high]>nums[high-1]) return high;
+        else{
+            low++;
+            high--;
         }
-        for(int i=1; i<=nums.length-2; i++){
-            if (i-1 == 0 && nums[i-1]>nums[i]) {
-                peak = i-1;
-                break;
-            }
-            if(nums[i-1]<nums[i] && nums[i+1] < nums[i]) {
-                peak = i;
-                break;
-            }
-            if(nums[i+1] == nums.length && peak == -1 && nums[i] < nums[i+1]) {
-                peak = i+1;
-                break;
-            }
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid-1] < nums[mid] && nums[mid+1] < nums[mid])
+                return mid;
+            if(nums[mid -1] < nums[mid]) low = mid+1;
+            else high = mid-1;
         }
-        return peak;
+        return -1;
     }
 }
