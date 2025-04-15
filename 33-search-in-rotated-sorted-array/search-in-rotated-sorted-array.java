0db2b158d3675b3cabbddaf76e6b67c8ea0edfcd
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-04-15 11:36:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (19.97%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,27 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
-        int low = 0;
-        int high = n-1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid]==target) return mid;
-            //Sorting property: (nums[low] <= nums[mid])
-            //Check for left sorted
-            if(nums[low]<=nums[mid]){
-                //Now check if the target exists in left sorted array.
-                if(nums[low]<=target && target<=nums[mid])
-                    high = mid-1;
-                else 
-                    low = mid+1;
-            } 
-            //else right array is sorted.
-            else{
-                //Check the target in right sorted array.
-                if(nums[mid]<=target && target <= nums[high])
-                    low = mid+1;
-                else 
-                    high = mid-1;
+        int left = 0;
+        int right = n-1;
+        //Binary search
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid] == target) return mid;
+            //Check for the sorting of the array.
+            //This is to see if left array is sorted.
+            if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && target<=nums[mid]){
+                    right = mid-1;
+                } else {
+                    left = mid+1;
+                }
+            } else {
+                if(nums[mid]<=target && target<=nums[right]){
+                    left = mid+1;
+                } else {
+                    right = mid-1;
+                }
             }
         }
         return -1;
