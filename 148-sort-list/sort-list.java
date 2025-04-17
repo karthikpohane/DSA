577dd2929ae66a3816823cbb92efed9675a3109b
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-04-18 00:22:43 +0530

    Time: 9 ms (94.74%) | Memory: 57.3 MB (23.41%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    // Merge two sorted linked lists
+    ListNode mergeTwoSortedLinkedLists(ListNode list1, ListNode list2) {
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+
+        if (list1 != null) {
+            temp.next = list1;
+        } else {
+            temp.next = list2;
+        }
+
+        return dummyNode.next;
+    }
+
+    // Find the middle node of the linked list
+    ListNode findMiddle(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    // Main sort function using merge sort
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode middle = findMiddle(head);
+        ListNode right = middle.next;
+        middle.next = null;
+        ListNode left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return mergeTwoSortedLinkedLists(left, right);
+    }
+}
