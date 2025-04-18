9fb56f69459695881d4ef75704ffa17fddf91d95
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-04-19 01:06:08 +0530

    Time: 5 ms (54.15%) | Memory: 42.9 MB (90.20%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
 
-        int[] count = new int[26]; // for lowercase letters only
-
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++; // increment count for s
-            count[t.charAt(i) - 'a']--; // decrement count for t
+        if(s.length() != t.length()) return false;
+        int[] seen = new int[26];
+        for(int i=0; i<s.length(); i++){
+            seen[s.charAt(i)-'a']++;
+            seen[t.charAt(i)-'a']--;
         }
-
-        // If all counts are zero, it's an anagram
-        for (int c : count) {
-            if (c != 0) return false;
+        for(int num: seen){
+            if(num!=0) return false;
         }
-
+        // for(int i=0; i<seen.length; i++){
+        //     if(seen[i] != 0) return false;
+        // }
         return true;
     }
 }
