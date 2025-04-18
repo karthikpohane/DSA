e05da726efd1a68ed7fabff008675ac20e343a4a
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-04-18 23:18:59 +0530

    Time: 3 ms (56.86%) | Memory: 43.5 MB (26.59%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -2,20 +2,18 @@ class Solution {
     public String countAndSay(int n) {
         if (n == 1) return "1";
 
-        String prev = countAndSay(n - 1);
-        StringBuilder result = new StringBuilder();
-
-        int count = 1;
-        for (int i = 1; i < prev.length(); i++) {
-            if (prev.charAt(i) == prev.charAt(i - 1)) {
+        String say = countAndSay(n - 1);
+        // Processing 
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < say.length(); i++) {
+            char ch = say.charAt(i);
+            int count = 1;
+            while (i < say.length() - 1 && say.charAt(i) == say.charAt(i + 1)) {
                 count++;
-            } else {
-                result.append(count).append(prev.charAt(i - 1));
-                count = 1;
+                i++;
             }
+            result.append(Integer.toString(count)).append(ch);
         }
-        // Append the last group
-        result.append(count).append(prev.charAt(prev.length() - 1));
 
         return result.toString();
     }
