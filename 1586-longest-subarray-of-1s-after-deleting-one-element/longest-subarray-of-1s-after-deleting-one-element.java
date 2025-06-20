19500a9debf11f63a941ee80b3f776af6da35922
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-06-20 12:04:19 +0530

    Time: 3 ms (79.28%) | Memory: 56.9 MB (48.32%) - LeetSync

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        // Sliding window using Acquire and Release approach
+        int i = 0;
+        int j = -1;
+        int ans = 0;
+        int coziw = 0;  // Count of zeros in current window
+
+        while (i < nums.length) {
+            // Acquire
+            if (nums[i] == 0) {
+                coziw++;
+            }
+            i++;
+
+            // Release until the window is valid (coziw <= k)
+            while (coziw > 1 && j < i) {
+                j++;
+                if (nums[j] == 0) {
+                    coziw--;
+                }
+            }
+
+            // Update max length of valid window
+            int currentWindowLength = i - j - 1;
+            ans = Math.max(ans, currentWindowLength);
+        }
+
+        return ans-1;
+    }
+}
