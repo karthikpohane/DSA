8375330ba24f598374bdf39025abe4d4089eea1d
Karthik Pohane <117158132+karthikpohane@users.noreply.github.com>
2025-06-20 12:59:15 +0530

    Time: 5 ms (40.81%) | Memory: 53.3 MB (29.88%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,22 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //Step 1 - Moore's voting algo
-        int count = 0;
-        int hero = nums[0];
-        for(int i=0; i<nums.length; i++){
-            if(count == 0){
-                count = 1;
-                hero = nums[i];
-            } else if(nums[i] == hero) count++;
-            else count--;
+        Arrays.sort(nums);
+        int ans = nums[0];
+        int freq = 0;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] == nums[i-1]){
+                freq++;
+            } else {
+                freq = 1;
+                ans = nums[i];
+            }
+            if(freq >= nums.length/2) return ans;
         }
-
-        //step 2 - verify if the hero ele occures more than n/2 times
-        int check = 0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == hero) check++;
-        }
-        if(check > (nums.length)/2) return hero;
-        return -1;
+        return ans;
     }
 }
